18ef49ee0854d20daff21a6942c277c42f0f2e5c
osteth <osteth@gmail.com>
2017-04-27 11:00:46 -0500

    added API smoke tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,3 +6,15 @@ def test_token_generator():
 	assert test_token is not ''
 	assert len(test_token) == 20
 	
+def test_checkinupdate():
+	data = api.CheckinUpdate('192.168.0.1', '5000', 'mudkips')
+	assert data is not None
+
+def test_checkin():
+	data = api.Player_Checkin()
+	assert data == {'Invalid': 'request'}
+
+	
+def test_STS():
+	data = api.STS()
+	assert data == {'Invalid': 'request'}
